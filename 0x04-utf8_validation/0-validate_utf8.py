cdcf9d5756be2cf41e9685945f21ba5bab1a16ea
olart <akanbi.ges201@gmail.com>
2022-07-15 11:09:41 +0100

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+main file for the utf-8 code
+"""
+
+
+def validUTF8(data):
+    """
+    function to return if an array of
+    integer is a valid UTF8 caharacter
+    """
+    def length(n):
+        return len('{:08b}'.format(n).split('0', 1)[0])
+
+    i = 0
+    while i < len(data):
+        le = length(data[i])
+        i += 1
+        if le == 1 or le > 4:
+            return False
+        if le > 1:
+            for _ in range(le - 1):
+                if i == len(data) or length(data[i]) != 1:
+                    return False
+                i += 1
+    return True
