130cd6bc891e3b4d9bdbcc07ea48a0883a219280
olart <akanbi.ges201@gmail.com>
2022-08-11 22:07:09 +0100

    0x08. Making Change

--- 0x08. Making Change/0-making_change.py
+++ 0x08. Making Change/0-making_change.py
@@ -10,10 +10,10 @@ def makeChange(coins, total):
     """
 
     dp = [total+1]*(total+1)
-        
+
     dp[0] = 0
     for a in range(total+1):
         for c in coins:
-          if a-c>=0:
-            dp[a]=min(dp[a],1+dp[a-c])
-    return dp[total] if dp[total]!=total+1 else -1
+            if a-c >= 0:
+                dp[a] = min(dp[a], 1+dp[a-c])
+    return dp[total] if dp[total] != total+1 else -1
